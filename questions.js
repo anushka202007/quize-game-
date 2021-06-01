1895aff9383280bb87735c160fa72d0f03ffc390
anushka202007 <71169908+anushka202007@users.noreply.github.com>
2021-06-01 17:11:40 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,199 @@
+var Questions = [
+  {
+    title: "Who was the 1st Chief Minister of Karnataka?",
+    choices: ["K. Chengalaraya Reddy", "K. Hanumanthaiah", "Kadidal Manjappa ", "R. Gundu Rao"],
+    answer: "K. Chengalaraya Reddy"
+  },
+  {
+    title: "When did Karnataka form a partial state of India ?",
+    choices: ["1 November, 1956", "12 November 1965", "15 October 1956", "15 October 1956"],
+    answer: "1 November, 1956"
+  },
+  {
+    title: "Who is known as the founder of Vijayanagara Empire?",
+    choices: ["Bukka Raya I ", "Harihara I", "Ramachandra Raya", "Deva Raya I "],
+    answer: "Harihara I"
+  },
+  {
+    title: "The Gol Gumbaz is located in the city? ",
+    choices: ["Davangere", "Raichur", "Gulbarga", "Bijapur"],
+    answer: "Bijapur"
+  },
+  {
+    title: "Nandi Hills, which is a famous tourist spot is located in which district of Karnataka?",
+    choices: ["Kolar", "Ramanagar","Chickaballapur","Tumkur"],
+    answer: "Chickaballapur"
+  },
+  {
+    title: "At which among the following places, there is no bench of Bombay High court?",
+    choices: ["Nasik", "Nagpur","Panji","Aurangabad"],
+    answer: "Nasik"
+  },
+  {
+    title: "Which of the following Dance form is Popular in Maharashtra state?",
+    choices: ["Lavani", "Kuchipudi","Mohiniattam","None of these"],
+    answer: "Lavani"
+  },
+  {
+    title: "Which city is famous as Orange city?",
+    choices: ["Nagpur", "Pune","Solapur","Nashik"],
+    answer: "Nagpur"
+  },
+  {
+    title: "The most important river of Maharashtra which is a lifeline of the whole South Maharashtra is? ",
+    choices: ["Koyna", "Krishna","Warna","Wardha"],
+    answer: "Krishna"
+  },
+  {
+    title: "Who was the first Chief Minister of Maharashtra ",
+    choices: ["Yashwantrao Chavan", "P. K. Sawant","Vasantrao Naik","Shankarrao Chavan"],
+    answer: "Yashwantrao Chavan"
+  },
+  {
+    title: "Who is the first woman Chief Minister of Delhi ",
+    choices: ["Sushma Swaraj", "Sheila Dikshit","Meira Kumar","Pratibha Patil"],
+    answer: "Sushma Swaraj"
+  },
+  {
+    title: "Is there a state located in the north, west, and south of Delhi?",
+    choices: ["Himachal Pradesh", "Uttar Pradesh","Haryana","Uttarakhand"],
+    answer: "Haryana"
+  },
+  {
+    title: "What was the old name of Delhi? ",
+    choices: ["Patliputra", "Magadh","Indraprastha","Hastinapur"],
+    answer: "Indraprastha"
+  },
+  {
+    title: "Who was the founder of Khilji Dynasty? ",
+    choices: ["Malik Firoz", "Jalal-ud-din Firoz Shah","Only A","Both A & B"],
+    answer: "Both A & B"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+  {
+    title: "Who built the Red Fort (Lal Quila) in Delhi?",
+    choices: ["Akbar", "Jahangir","Shahjahan","Aurangzeb"],
+    answer: "Shahjahan"
+  },
+
+];
+
